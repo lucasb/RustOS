9ed41fe112f0d11e468ffb5c4ce15e8cbf8f4917
Lucas Boscaini <lucasboscaini@gmail.com>
2024-04-15 03:26:48 +0000

    wip - post 02

--- install_rust.sh
+++ install_rust.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
+# install rust
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | \
 bash -s -- -y --no-modify-path
 
+# set env to path
 . "$HOME/.cargo/env"
+
+# add targets
+rustup target add x86_64-unknown-none
+rustup target add aarch64-unknown-none
